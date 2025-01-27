5e62a1429fd82a35bbb470e06e0f305c66595c1c
DAVIDPRENDERGAST-STU <g00405007@atu.ie>
2025-01-27 11:23:39 +0000

    First Commit

---
+++ src/main/java/ie/week/week/sem2/FoodApp.java
@@ -0,0 +1,13 @@
+package ie.week.week.sem2;
+
+public class FoodApp {
+
+    public static void main(String[]args) {
+        System.out.println("Welcome to Just App");
+        System.out.println("This is your order");
+        //Create an order
+
+
+        //List all of the food items burgers, pizzas ,fries, etc
+    }
+}
